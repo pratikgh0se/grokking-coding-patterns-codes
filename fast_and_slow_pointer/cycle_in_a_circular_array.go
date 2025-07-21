f7d7bb0c843e02616407aa0db912145cab720993
pratikgh0se <pratikghose1999@gmail.com>
2025-07-21 20:21:10 +0530

    Day 3

--- fast_and_slow_pointer/cycle_in_a_circular_array.go
+++ fast_and_slow_pointer/cycle_in_a_circular_array.go
@@ -3,33 +3,55 @@ type Solution struct{}
 func (s *Solution) loopExists(arr []int) bool {
 	// TODO: Write your code here
 	n := len(arr)
-	visited := make([]bool, n)
+	visitedForward := make([]bool, n)
+	visitedBackward := make([]bool, n)
 
 	for i, _ := range arr {
-		if visited[i] {
-			continue
-		}
 		nextIdx := getNextIndex(i, arr[i], n)
 		if nextIdx == i {
 			continue
 		}
 		slow, fast := i, i
 		currForwardOrNot := isFoward(arr[i])
+		if currForwardOrNot {
+			if visitedForward[i] {
+				continue
+			}
+		} else {
+			if visitedBackward[i] {
+				continue
+			}
+		}
 		for slow < n && fast < n {
-			visited[slow] = true
-			visited[fast] = true
+			if currForwardOrNot {
+				visitedForward[slow] = true
+				visitedForward[fast] = true
+			} else {
+				visitedBackward[slow] = true
+				visitedBackward[fast] = true
+			}
+
 			slow = getNextIndex(slow, arr[slow], n)
 			if currForwardOrNot != isFoward(arr[slow]) {
 				break
 			}
+
 			fastNext := getNextIndex(fast, arr[fast], n)
 			if currForwardOrNot != isFoward(arr[fastNext]) {
 				break
 			}
+
+			if currForwardOrNot {
+				visitedForward[fastNext] = true
+			} else {
+				visitedBackward[fastNext] = true
+			}
+
 			fast = getNextIndex(fastNext, arr[fastNext], n)
 			if currForwardOrNot != isFoward(arr[fast]) {
 				break
 			}
+
 			if slow == fast {
 				nextPos := getNextIndex(slow, arr[slow], n)
 				if nextPos == slow {
@@ -37,6 +59,7 @@ func (s *Solution) loopExists(arr []int) bool {
 				}
 				return true
 			}
+
 		}
 	}
 	return false
